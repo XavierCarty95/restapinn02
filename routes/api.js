66f62542b3b6e41bca8f8064b822ab5a395fea2c
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-15 23:59:03 +0000

    post man

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,13 @@ router.get('/stocks', function(req,res){
 })
 
 router.post('/stocks', function(req,res){
-    res.send({type : 'POST'})
+    console.log(req.body)
+    res.send({
+        
+        type : 'POST', 
+        name: req.body.name, 
+        rank: req.body.price
+    })
 })
 
 router.put('/stocks/:id', function(req,res){
