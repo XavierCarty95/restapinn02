12cc0c9c69c99c2b165439929724ca4d229bcbd0
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-16 00:41:34 +0000

    Setting up models and schemas

---
+++ ninjas.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+
+// creat stock schema & models 
+
+const StockSchema = new Schema({
+    
+    name: {
+        type: String,
+        required: [true, "Name field is required"]
+    }, 
+    
+    price: { 
+        type: Number
+        
+    }, 
+    
+    availabile: {
+        type : Boolean, 
+        default: false
+    }
+})
+
+const Stock = mongoose.model('stock', StockSchema);
+
+
+
+module.exports = Stock
